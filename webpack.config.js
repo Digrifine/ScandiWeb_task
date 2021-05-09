65bba86d82654a1a0ad301fe426fd539340f758f
Digrifine <digrifine@gmail.com>
2021-05-09 17:59:16 +0300

    clear useless packages

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,6 @@ let conf = {
 			},
 			{
 				test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-				// More information here https://webpack.js.org/guides/asset-modules/
 				type: 'asset',
 			},
 		],
